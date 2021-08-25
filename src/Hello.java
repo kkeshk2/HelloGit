4e0d80ccb36997ff0fc05f071a0a09fe24024e27
karee <karee@DESKTOP-PUP0614.unl.edu>
2021-08-25 15:15:44 -0500

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * Author: Kareem Keshk (kkeshk2@huskers.unl.edu)
+ * Date: 08/25/2021
+ * 
+ * A simple "Hello World!" program in Java
+ * 
+ * @author kkeshk2
+ *
+ */
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Kareem Keshk");
+	}
+
+}
